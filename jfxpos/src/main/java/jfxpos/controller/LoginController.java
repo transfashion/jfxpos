802c1c88545955dd0ac59882313f4d03d9e42e3a
agung-transfashion <agung@transfashionindonesia.com>
2026-03-17 11:48:21 +0700

    checkpoint 20260317 1148

--- jfxpos/src/main/java/jfxpos/controller/LoginController.java
+++ jfxpos/src/main/java/jfxpos/controller/LoginController.java
@@ -1,6 +1,11 @@
 package jfxpos.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import jfxpos.util.ErrorMessage;
 import jfxpos.util.WindowManager;
@@ -8,6 +13,12 @@ import jfxpos.util.WindowManager;
 public class LoginController {
 
 	Stage stage;
+
+	@FXML
+	private Label txtVersion;
+
+	@FXML
+	private Label txtNamedVersion;
 
 	public void setStage(Stage stage) {
 		this.stage = stage;
@@ -38,4 +49,30 @@ public class LoginController {
 		}
 	}
 
+	@FXML
+	public void initialize() {
+		Properties p = new Properties();
+
+		try (InputStream is = getClass().getResourceAsStream("/app.properties")) {
+			p.load(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		String version = p.getProperty("app.version");
+		String namedversion = p.getProperty("app.namedversion");
+
+		if (version == null) {
+			txtVersion.setText("DEV");
+		} else {
+			txtVersion.setText("v" + version);
+		}
+
+		if (namedversion == null) {
+			txtNamedVersion.setText("");
+		} else {
+			txtNamedVersion.setText("- " + namedversion);
+		}
+
+	}
 }

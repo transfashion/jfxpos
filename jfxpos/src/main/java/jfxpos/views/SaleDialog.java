6df2e40484d688bd52415940a069d4b36bdaaed6
agung-transfashion <agung@transfashionindonesia.com>
2026-07-08 17:57:50 +0700

    feat: implement SaleDialog view and update project version to 1.1.260708

--- jfxpos/src/main/java/jfxpos/views/SaleDialog.java
+++ jfxpos/src/main/java/jfxpos/views/SaleDialog.java
@@ -137,7 +137,16 @@ public class SaleDialog extends View {
 
 		// Execute escButton when Escape key is pressed
 		scene.addEventFilter(javafx.scene.input.KeyEvent.KEY_PRESSED, event -> {
-			if (event.getCode() == javafx.scene.input.KeyCode.ESCAPE) {
+			if (event.isControlDown() && event.getCode() == javafx.scene.input.KeyCode.V) {
+				if (!controller.isLineInputFocused()) {
+					javafx.scene.input.Clipboard clipboard = javafx.scene.input.Clipboard.getSystemClipboard();
+					if (clipboard.hasString()) {
+						controller.requestFocus();
+						controller.appendLineInput(clipboard.getString());
+					}
+					event.consume();
+				}
+			} else if (event.getCode() == javafx.scene.input.KeyCode.ESCAPE) {
 				controller.fireEscButton();
 				event.consume();
 			} else if (event.getCode() == javafx.scene.input.KeyCode.F1) {
